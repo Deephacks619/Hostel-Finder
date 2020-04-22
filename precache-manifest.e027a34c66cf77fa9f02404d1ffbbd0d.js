b60fc7f9f89c0dd39d331c61bcf2859d0dc0db81
deephacks619 <deepakkyatham@gmai.com>
2020-04-22 13:22:30 +0530

    Updates

---
+++ precache-manifest.e027a34c66cf77fa9f02404d1ffbbd0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a70be0b0909d93b4db8bdb718127541",
+    "url": "/Hostel-Finder/index.html"
+  },
+  {
+    "revision": "3ae64ab898abe330ae8f",
+    "url": "/Hostel-Finder/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "d12b94bce4684cc6adcc",
+    "url": "/Hostel-Finder/static/css/main.0e286851.chunk.css"
+  },
+  {
+    "revision": "3ae64ab898abe330ae8f",
+    "url": "/Hostel-Finder/static/js/2.842efe6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Hostel-Finder/static/js/2.842efe6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12b94bce4684cc6adcc",
+    "url": "/Hostel-Finder/static/js/main.8de3b42f.chunk.js"
+  },
+  {
+    "revision": "70d87b6596e201df8b12",
+    "url": "/Hostel-Finder/static/js/runtime-main.265aa42b.js"
+  }
+]);
